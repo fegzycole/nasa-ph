73fdb8793d373201ed3640821252e9e716eab8d0
ferguson <fergusoniyara@gmail.com>
2020-06-27 03:04:50 +0100

    Render header dynamically

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import Header from './Header';
 import Home from '../containers/Home';
 import appStyles from '../styles/app.module.scss';
 
-function App() {
+function App({ pictures }) {
+  const picture = pictures.find(pic => pic.favorite === true);
+
   return (
     <div className={appStyles.app}>
-      <Header />
+      {
+        picture ? <Header /> : null
+      }
 
       <Switch>
         <Route path="/" component={Home} />
@@ -17,4 +23,12 @@ function App() {
   );
 }
 
-export default App;
+const mapStateToProps = ({ pictures }) => ({
+  pictures,
+});
+
+App.propTypes = {
+  pictures: PropTypes.instanceOf(Array).isRequired,
+};
+
+export default connect(mapStateToProps)(App);
